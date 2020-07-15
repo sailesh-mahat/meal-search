b1b3d43a9cbae9f927fef8cd5ae03fe5f64afb28
sailesh-mahat <sailesharsenal@gmail.com>
2020-07-15 19:05:13 +0200

    Created Search component

---
+++ meal-search/src/Search.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+
+    state = {};
+    render() {
+        return <h1>Welcome to the meal search</h1>;
+    }
+}
+
+export default Search;
