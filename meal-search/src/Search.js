bd3ebd36e9dfe8cbd2e2836008e47c8c5e059e32
sailesh-mahat <sailesharsenal@gmail.com>
2020-07-15 19:19:04 +0200

    added onChnage listener and value attirbute

--- meal-search/src/Search.js
+++ meal-search/src/Search.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
+import './Search.css';
 
 class Search extends Component {
 
-    state = {};
+    state = {
+        searchValue: '',
+    };
     render() {
         return (
             <div>
                 <h1>Welcome to the meal search</h1>
-                <input name="text" type="text" placeholder="Search" />
+                <input name="text" 
+                type="text" 
+                placeholder="Search" 
+                onChange={event=> this.handleOnChange(event)}
+                value={this.state.searchValue}
+                />
                 <button>Search</button>
             </div>
         );
